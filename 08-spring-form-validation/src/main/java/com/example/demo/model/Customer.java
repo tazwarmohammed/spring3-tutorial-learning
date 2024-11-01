516096bcb0af82b87815994e2561352826701352
Tazwar Mohammed Shoumik <tazwarmohammed1998@gmail.com>
2024-11-02 00:06:27 +0600

    ADD regex validation

--- 08-spring-form-validation/src/main/java/com/example/demo/model/Customer.java
+++ 08-spring-form-validation/src/main/java/com/example/demo/model/Customer.java
@@ -10,12 +10,17 @@ public class Customer {
 	private String firstName;
 
 	@NotNull(message = "Last name is required")
-//	@NotBlank
+//	@NotBlank(message = "Last name is required")
 	@Size(min = 1, message = "Last name must be at least 1 character long")
 	private String lastName;
 
 	@NotNull(message = "Age is required")
+//	@NotBlank(message = "Age is required")
 	@Min(value = 18, message = "Age must be at least 18")
 	@Max(value = 65, message = "Age must be less than 65")
-	private Integer age;
+	@Pattern(regexp = "^\\d{2}$", message = "Age must be a number between 18 and 65")
+	private String age;
+
+	@Pattern(regexp = "^\\d{11}$", message = "Phone number must be 11 characters long and contain only numbers")
+	private String phoneNumber;
 }
