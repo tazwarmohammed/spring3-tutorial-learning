5757a7359194c197b99867c8bf002c5225bbc4e7
Tazwar Mohammed Shoumik <tazwarmohammed1998@gmail.com>
2024-11-13 00:04:46 +0600

    ADD custom annotation class for custom form validation

---
+++ 08-spring-form-validation/src/main/java/com/example/demo/annotation/CourseCode.java
@@ -0,0 +1,20 @@
+package com.example.demo.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+	public String value() default "CSE";
+	public String message() default "Must start with CSE";
+	public Class<?>[] groups() default {};
+	public Class<? extends Payload>[] payload() default {};
+}
